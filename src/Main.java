c0afe14335b19cf7c0b6d42ab547807c4075590c
Ben Maydan <maydanb@gmail.com>
2020-12-14 12:13:14 -0800

    input

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,35 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String inp = printInput(scanner, "Welcome to the game! Now you've lost the game!");
+
+        String inp = printInput(scanner, "Welcome to the game! Now you've lost the game!", new String[] {"Continue"});
     }
 
-    public static String printInput(Scanner s, String msg) {
-        System.out.println(msg + "\nInput: ");
+    public static String printInput(Scanner s, String msg, String[] responses) {
+        System.out.print(msg + "\nInput (" + responsesToString(responses) + "): ");
+
+        String inp = s.next();
+        while (!inside(inp, responses)) {
+            System.out.println();
+            inp = s.next();
+        }
+
         return s.next();
+    }
+
+    public static boolean inside(String a, String[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            if (a.equals(arr[i]))
+                return true;
+        return false;
+    }
+
+    public static String responsesToString(String[] arr) {
+        String out = "";
+        for (int i = 0; i < arr.length; i++) {
+            out += arr[i] + "/";
+        }
+        return out;
     }
 
 }
